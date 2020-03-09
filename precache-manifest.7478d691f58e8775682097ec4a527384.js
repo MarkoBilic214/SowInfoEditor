6218db4e7cf00b9bb858a6ded6b8ff211f8cbd40
MarkoBilic214 <mbilic@ryerson.ca>
2020-03-09 13:21:07 -0400

    Updates

---
+++ precache-manifest.7478d691f58e8775682097ec4a527384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19871913da110c2fb641a170aa7c2338",
+    "url": "/sow-info-editor/index.html"
+  },
+  {
+    "revision": "e0b0a868b2f094fe8d35",
+    "url": "/sow-info-editor/static/css/main.42ab5819.chunk.css"
+  },
+  {
+    "revision": "c2f7d263861c7135c475",
+    "url": "/sow-info-editor/static/js/2.a2667eb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sow-info-editor/static/js/2.a2667eb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0b0a868b2f094fe8d35",
+    "url": "/sow-info-editor/static/js/main.73508f09.chunk.js"
+  },
+  {
+    "revision": "97e13b50f34ef45ab3ef",
+    "url": "/sow-info-editor/static/js/runtime-main.ec8ffdc1.js"
+  }
+]);
